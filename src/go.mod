b72b449f6079c788ca44d45f8ea2ee9a083b1a93
atiagosoares <andre.soares@altxtech.net>
2024-03-17 16:06:00 -0300

    Initial package

--- src/go.mod
+++ src/go.mod
@@ -1,8 +1,12 @@
-module template/cloudrun
+module smartgrowth-connectors/configapi
 
 go 1.20
 
-require github.com/gin-gonic/gin v1.9.1
+require (
+	github.com/auth0/go-jwt-middleware/v2 v2.2.1
+	github.com/gin-gonic/gin v1.9.1
+	github.com/google/uuid v1.6.0
+)
 
 require (
 	github.com/bytedance/sonic v1.9.1 // indirect
@@ -23,10 +27,12 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/go-jose/go-jose.v2 v2.6.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -1,19 +1,37 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"net/http"
+	"log"
+	"smartgrowth-connectors/configapi/controller"
+	"smartgrowth-connectors/configapi/database"
+	"smartgrowth-connectors/configapi/server"
 )
 
-func helloWorld(c *gin.Context){
-	c.String(http.StatusOK, "Your Cloud Run app was deployed successfully!")
-}
+/*
+Main package should be responsible for initializing the app.
 
-func main() {
+Using enviroment variables to initialize the database, configure the controller, start the app, etc..
+*/
+
+func main(){
+
+	// Initialize database
+	db, err := database.NewInMemoryDB()
+	if err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	}
+
+
+	// Configure controller
+	controller, err := controller.NewController(db)
+	if err != nil {
+		log.Fatalf("Failed to initialize controller: %v", err)
+	}
 	
-	app := gin.Default()
-	app.GET("/", helloWorld)
+	server, err := server.NewServer(controller, "dev-sn5f570cadx2zciy", "https://api.connectors.smartgrowth.consulting")
+	if err != nil {
+		log.Fatalf("Error initializing server: %v", err)
+	}
 
-
-	app.Run()
+	server.Run()
 }
